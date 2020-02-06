f655f2a760a0ecd3a4f6dd90466203c21ea06010
Cédric Clavier <cedric@saooti.com>
2020-02-06 16:14:29 +0100

    Ajustement de la taille du bouton plus

--- src/components/display/categories/CategoryList.vue
+++ src/components/display/categories/CategoryList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-inline-flex w-100 mb-3 pl-3 pr-3 hide-phone">
+  <div class="d-inline-flex w-100 mb-3 pl-3 pr-3 hide-phone category-list">
     <div class="category-list-container" id="category-list-container">
       <router-link
         :id="'category'+category.id"
@@ -7,19 +7,24 @@
         class="category-item text-dark secondary-bg"
         v-for="category in categories"
         v-bind:key="category.id"
-        >{{ category.name }}</router-link
-      >
+      >{{ category.name }}</router-link>
     </div>
-    <b-dropdown v-show="hidenCategories.length" right toggle-class="text-decoration-none text-dark category-item" no-caret>
+    <b-dropdown
+      v-show="hidenCategories.length"
+      right
+      toggle-class="text-decoration-none text-dark category-item category-item-plus"
+      no-caret
+    >
       <template v-slot:button-content>
-          <i class="saooti-plus"></i>
+        <i class="saooti-plus"></i>
       </template>
       <template>
-        <b-dropdown-item :to="'/main/pub/category/' + category.id" 
-            v-for="category in hidenCategories" 
-            v-bind:key="category.id" 
-            class="mr-3"
-          >{{ category.name }}</b-dropdown-item>
+        <b-dropdown-item
+          :to="'/main/pub/category/' + category.id"
+          v-for="category in hidenCategories"
+          v-bind:key="category.id"
+          class="mr-3"
+        >{{ category.name }}</b-dropdown-item>
       </template>
     </b-dropdown>
   </div>
@@ -50,67 +55,78 @@
     background: #ddd !important;
   }
 }
+
+.category-list .category-item-plus {
+  display: flex;
+  height: 1.5rem;
+  width: 1.5rem;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.5rem;
+}
 </style>
 <script>
-import {state} from "../../../store/paramStore.js";
+import { state } from "../../../store/paramStore.js";
 
 export default {
-  name: 'CategoryList',
+  name: "CategoryList",
 
-  mounted(){
-    window.addEventListener('resize', this.resizeWindow);
+  mounted() {
+    window.addEventListener("resize", this.resizeWindow);
     this.resizeWindow();
   },
 
- data() {
+  data() {
     return {
-      hidenCategories : [],
+      hidenCategories: []
     };
   },
 
   computed: {
-    categories(){
+    categories() {
       return state.generalParameters.allCategories.filter(c => {
-          return c.podcastCount;
-        });
+        return c.podcastCount;
+      });
     }
   },
 
-  methods:{
-    resizeWindow(){
-      document.getElementById("category-list-container").style.justifyContent = 'flex-start';
+  methods: {
+    resizeWindow() {
+      document.getElementById("category-list-container").style.justifyContent =
+        "flex-start";
       this.hidenCategories = [];
       this.categories.forEach(element => {
-        let el = document.getElementById('category' + element.id);
-        if(el){
+        let el = document.getElementById("category" + element.id);
+        if (el) {
           const parent = el.parentNode;
-          if(el.offsetLeft + el.clientWidth > parent.clientWidth - 20){
+          if (el.offsetLeft + el.clientWidth > parent.clientWidth - 20) {
             this.hidenCategories.push(element);
-            if(!el.classList.contains('hid')){
-              el.className += ' hid';
+            if (!el.classList.contains("hid")) {
+              el.className += " hid";
             }
           } else {
-            el.classList.remove('hid');
+            el.classList.remove("hid");
           }
         }
       });
-      if(!this.hidenCategories.length){
-        document.getElementById("category-list-container").style.justifyContent = 'center';
+      if (!this.hidenCategories.length) {
+        document.getElementById(
+          "category-list-container"
+        ).style.justifyContent = "center";
       }
     }
   },
 
   beforeDestroy() {
-    window.removeEventListener('resize', this.resizeWindow);
+    window.removeEventListener("resize", this.resizeWindow);
   },
 
-  watch:{
-    categories(){
-      this.$nextTick(()=>{
-        this.resizeWindow(); 
-      });   
+  watch: {
+    categories() {
+      this.$nextTick(() => {
+        this.resizeWindow();
+      });
     }
   }
-
 };
 </script>
