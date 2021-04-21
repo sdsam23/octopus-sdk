e5f3ec973e35b7e77c3c93b1d54411f100858080
CamilleSaulnier <camille@saooti.com>
2021-04-21 10:42:50 +0200

    Traduction

---
+++ src/locale/educationen.ts
@@ -0,0 +1,14 @@
+export default{
+    Productors: 'Educational organizations',
+    Productor: 'Educational organization',
+    'Number productors': '{nb} educational organizations',
+    'select productor': 'Select educational organization',
+    'Please chose a productor': 'Please chose an educational organization',
+    'All productors': 'All educational organizations',
+    "Look for productor name": 'Search for educational organization by name',
+    'Type string to filter by organisation': 'Filter educational organization by name',
+    'No organisation filter': 'No educational organization filter',
+    'Loading productors ...': 'Loading educational organizations...',
+    "Productor doesn't exist": "Educational organization doesn't exist",
+  };
+  
--- src/locale/messages.ts
+++ src/locale/messages.ts
@@ -1,9 +1,11 @@
 import fr from './fr';
 import educationfr from './educationfr';
+import educationen from './educationen';
 import en from './en';
 
 export default {
   fr: fr,
   en: en,
   educationfr: educationfr,
+  educationen: educationen,
 };
